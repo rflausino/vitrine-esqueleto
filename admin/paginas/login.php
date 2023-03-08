f34b7f6989c7f7131bda3c0354a1ed20c8dc705b
rflausino <renanflausino2000@gmail.com>
2023-03-08 20:21:10 -0300

    Alteração 2° aula

---
+++ admin/paginas/login.php
@@ -0,0 +1,16 @@
+<div class="login">
+    <h1 class="text-center">Efetuar Login</h1>
+    <form action="">
+        <label for="login">Login</label>
+        <input type="text" name="login" id="login" class="form-control" require placeholder="Por favor preencha esta campo">
+
+        <br>
+
+        <label for="senha">Senha</label>
+        <input type="password" name="sanha" id="senha" class="form-control" require placeholder="Por favor preencha este campo">
+        <br>
+
+        <button type="submit" class="btn btn-success w-100">Efetuar Login</button>
+        
+    </form>
+</div>
